4e5f0737ae50aef95dcdd072675d3dad076552da
sr.davidsantiago@hotmail.com <sr.davidsantiago@hotmail.com>
2024-11-22 11:01:41 -0500

    Creación de Proyecto junto con ejemplos

---
+++ src/EjemploCuatro/ParteDos/Calculadora.java
@@ -0,0 +1,7 @@
+package EjemploCuatro.ParteDos;
+
+public class Calculadora {
+    public static int sumar(int a, int b) {
+    return a + b;
+    }
+}
---
+++ src/EjemploCuatro/ParteUno/Main.java
@@ -0,0 +1,11 @@
+package EjemploCuatro.ParteUno;
+
+public class Main {
+    public static void main(String[] args) {
+    Contador c1 = new Contador();
+    Contador c2 = new Contador();
+    Contador c3 = new Contador();
+    // Se accede al método estático usando el nombre de la clase
+    Contador.mostrarContador(); // Muestra: Contador global: 3
+    }
+}

10ff56473e07c596e91d81b6976be711f33744e8
sr.davidsantiago@hotmail.com <sr.davidsantiago@hotmail.com>
2024-11-22 23:47:00 -0500

    Modificacion final

--- src/Coche.java
+++ src/Coche.java
@@ -1,3 +1,44 @@
 public class Coche {
+    //Atributos de la clase
+    private String marca;
+    private String modelo;
 
+    //Atributo estático para contar las instancias
+    private static int contadorCoches = 0;
+
+    //Constructor
+    public Coche(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+        //Incrementa el contador cada vez que se crea un objeto
+        contadorCoches++;
+    }
+
+    //Método estático
+    public static int mostrarContadorCoches() {
+        return contadorCoches;
+    }
+
+    //Método toString para mostrar la información del coche
+    @Override
+    public String toString() {
+        return "Coche [marca=" + marca + ", modelo=" + modelo + "]";
+    }
+
+
+    //Main para probar la clase
+    public static void main(String[] args) {
+       
+        Coche coche1 = new Coche("Toyota", "Corolla");
+        Coche coche2 = new Coche("Ford", "Focus");
+        Coche coche3 = new Coche("Honda", "Civic");
+
+        //Mostrar la información de cada coche
+        System.out.println(coche1);
+        System.out.println(coche2);
+        System.out.println(coche3);
+
+        //Mostrar cuántos coches se han creado
+        System.out.println("Coches creados: " + Coche.mostrarContadorCoches());
+    }
 }
--- src/UsoIncorrectoStatic.java
+++ src/UsoIncorrectoStatic.java
@@ -1,3 +1,14 @@
 public class UsoIncorrectoStatic {
+    // Atributo no estático
+    private static int contador = 0;
 
+    // Método estático intentando acceder al atributo no estático
+    public static void incrementarContador() {
+        contador++; // Error de compilación
+    }
+
+    public static void main(String[] args) {
+        // Llamar al método estático
+        incrementarContador();
+    }
 }
